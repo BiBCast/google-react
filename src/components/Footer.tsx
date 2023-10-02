a4c301777351b74e54d3139ec04400a3fbaa52e1
BiBCast <pietrocast03@gmail.com>
2023-10-02 23:26:46 +0200

    [ADD] general style , add icon serach bar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,22 @@
 export function Footer() {
   return (
     <div className="footer">
-      <div className="footer__row">
-        <div className="row__element">
-          <span>8</span>
+      <div className="footer__row  border_bottom">
+        <div className="row__element ">
+          <span>Italy</span>
         </div>
       </div>
       <div className="footer__row">
         <div className="row__element">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
+          <span>About</span>
+          <span>Advertising</span>
+          <span>Business</span>
+          <span>How the search works</span>
         </div>
         <div className="row__element">
-          <span>5</span>
-          <span>6</span>
-          <span>7</span>
+          <span>Privacy</span>
+          <span>Term</span>
+          <span>Settings</span>
         </div>
       </div>
     </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,26 @@
+import { Glass } from "../assets/Glass";
+import { Tv } from "../assets/Tv";
+
 export function SearchBar() {
   return (
     <div className="search_bar">
       <div className="search_bar__container">
-        <div className="icon">icon</div>
+        <div className="icon">
+          <Glass />
+        </div>
         <input className="input" type="search" />
-        <div>X</div>
+        <div className="gray">X</div>
         <div className="container__separator"></div>
-        <div className="icon">icon</div>
+        <div className="icon">
+          <Tv />
+        </div>
       </div>
       <div className="search_bar__button">
         <div>Google search</div>
         <div>i'm feeling lucky</div>
       </div>
       <div className="search_bar__language">
-        Google offered in :<a href="#">italiano</a>
+        Google offered in:<a href="#">&nbsp;italiano</a>
       </div>
     </div>
   );
